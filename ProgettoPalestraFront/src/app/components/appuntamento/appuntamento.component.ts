66c90ac21535a1a2d7113d66a88188d805f96757
MishelCakalli <mishelcakalli@gmail.com>
2024-07-08 22:05:59 +0200

    commit pulsante elimina su appuntamenti

--- ProgettoPalestraFront/src/app/components/appuntamento/appuntamento.component.ts
+++ ProgettoPalestraFront/src/app/components/appuntamento/appuntamento.component.ts
@@ -7,7 +7,7 @@ import { Appuntamento } from '../../interfaces/appuntamento';
   templateUrl: './appuntamento.component.html',
   styleUrls: ['./appuntamento.component.scss']
 })
-export class AppuntamentoComponent implements OnInit{
+export class AppuntamentoComponent implements OnInit {
   appuntamenti: Appuntamento[] = [];
 
   constructor(private appuntamentoService: AppuntamentoService) {}
@@ -19,6 +19,17 @@ export class AppuntamentoComponent implements OnInit{
       },
       error => {
         console.error('Errore nel recupero degli appuntamenti', error);
+      }
+    );
+  }
+
+  eliminaAppuntamento(id: number, indice: number): void {
+    this.appuntamentoService.deleteAppuntamento(id).subscribe(
+      () => {
+        this.appuntamenti.splice(indice, 1);
+      },
+      error => {
+        console.error('Errore durante l\'eliminazione dell\'appuntamento', error);
       }
     );
   }
